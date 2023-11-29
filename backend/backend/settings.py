f32c8e20ae9d36963ed6c8ababf179fbbb704e1e
Сойта Павел <soita.pavel@yandex.ru>
2023-11-29 17:50:48 +0300

    dotenv

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8xsor1=+@n7a4#h%s(n&7@b3w6qzzza74vi-p742ts1fq2(%2c'
+SECRET_KEY = os.getenv('SECRET_KEY', default='token')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['51.250.25.216', '127.0.0.1', 'localhost', 'soitafoodgramm.ddns.net']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS',
+                          default='127.0.0.1, localhost').split()
 
 
 # Application definition
