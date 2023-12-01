85f76ebdc84dd81e0e6a05fc092712df7470d33e
Сойта Павел <soita.pavel@yandex.ru>
2023-12-01 18:28:04 +0300

    correct ser-r

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -84,13 +84,6 @@ class RecipeShowSerializer(serializers.ModelSerializer):
                   'is_in_shopping_cart', 'name', 'image', 'text',
                   'cooking_time')
 
-    # def get_is_favorited(self, obj):
-    #     user = self.context.get('request').user
-    #     return (user.favorites.filter(recipe=obj).exists())
-
-    # def get_is_in_shopping_cart(self, obj):
-    #     user = self.context.get('request').user
-    #     return (user.shopping_cart.filter(recipe=obj).exists())
     def get_is_favorited(self, obj):
         return self.obj_exists(obj, Favorite)
 
