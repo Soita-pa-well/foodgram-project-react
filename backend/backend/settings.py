3ca12f7bff4a5ac6717bf2d06b98a09a2b2442c0
Сойта Павел <soita.pavel@yandex.ru>
2023-12-04 20:11:41 +0300

    settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -138,7 +138,7 @@ DJOSER = {
         'current_user': 'api.serializers.CustomUserSerializer',
     },
     'PERMISSIONS': {
-        'user': ['djoser.permissions.CurrentUserOrAdminOrReadOnly'],
+        'user': ['rest_framework.permissions.AllowAny'],
         'user_list': ['rest_framework.permissions.AllowAny']
     },
 }
