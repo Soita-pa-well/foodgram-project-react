df54ec9723c48acde3eb27bb9e920c5174563112
Сойта Павел <soita.pavel@yandex.ru>
2023-12-04 23:38:57 +0300

    superuser

--- backend/users/managers.py
+++ backend/users/managers.py
@@ -10,3 +10,12 @@ class UserManager(BaseUserManager):
         user.set_password(password)
         user.save()
         return user
+
+    def create_superuser(self, email, username, password, **extra_fields):
+        extra_fields.setdefault('is_superuser', True)
+
+        if not extra_fields.get('is_superuser'):
+            raise ValueError(
+                'Суперпользователь должен иметь is_superuser=True!'
+            )
+        return self.create_user(email, username, password, **extra_fields)
